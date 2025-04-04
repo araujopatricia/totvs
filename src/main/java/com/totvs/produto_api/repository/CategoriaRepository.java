25a60368dfb719f2ffaef52c65c46e0c72e626ad
patricia <araujo.patricia.sa@gmail.com>
2025-04-04 11:58:57 -0300

    adiciona controllers e dto e fix no repositorio

--- src/main/java/com/totvs/produto_api/repository/CategoriaRepository.java
+++ src/main/java/com/totvs/produto_api/repository/CategoriaRepository.java
@@ -1,5 +1,6 @@
 package com.totvs.produto_api.repository;
 
+import com.totvs.produto_api.dto.CategoriaDTO;
 import com.totvs.produto_api.modelo.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,5 +12,5 @@ import java.util.List;
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
     @Query("SELECT new com.teste.produtoapi.dto.Categoria(c.nome, SUM(p.quantidadeEstoque)) " +
             "FROM Categoria c JOIN c.produtos p GROUP BY c.nome")
-    List<Categoria> findEstoqueTotalPorCategoria();
+    List<CategoriaDTO> findEstoqueTotalPorCategoria();
 }
