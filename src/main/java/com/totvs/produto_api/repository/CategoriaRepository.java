4141426b42e18b4c19643a17370fa47ce25be38f
patricia <araujo.patricia.sa@gmail.com>
2025-04-04 11:34:18 -0300

    adiciona repositorios

---
+++ src/main/java/com/totvs/produto_api/repository/CategoriaRepository.java
@@ -0,0 +1,15 @@
+package com.totvs.produto_api.repository;
+
+import com.totvs.produto_api.modelo.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+    @Query("SELECT new com.teste.produtoapi.dto.Categoria(c.nome, SUM(p.quantidadeEstoque)) " +
+            "FROM Categoria c JOIN c.produtos p GROUP BY c.nome")
+    List<Categoria> findEstoqueTotalPorCategoria();
+}
